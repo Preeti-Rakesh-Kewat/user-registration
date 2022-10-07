c113c030df440abc328fca018071e7e5d13e278c
preetirkewat@gmail.com <preetirkewat@gmail.com>
2022-10-07 13:12:38 -0400

    cleaned up unused method

--- src/main/java/com/pnc/registration/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pnc/registration/exception/GlobalExceptionHandler.java
@@ -6,15 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
-import javax.validation.Path;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 @Data
@@ -22,30 +16,6 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 @EqualsAndHashCode(callSuper = false)
 public class GlobalExceptionHandler  {
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> constraintValidationException(ConstraintViolationException constraintViolationException) {
-        String errorMessage = "";
-        if (constraintViolationException != null && constraintViolationException.getConstraintViolations() != null) {
-            //Gets the first violation in the list
-            ConstraintViolation<?> violation = constraintViolationException.getConstraintViolations().iterator().next();
-
-            //Get the last node of the violation
-            String field = null;
-
-            //The returned value of getPropertyPath() is an Iterable<Node> and the last element of the iterator is the field name that violated the constraint.
-            for (Path.Node node : violation.getPropertyPath()) {
-                field = node.getName();
-            }
-
-            StringJoiner joiner = new StringJoiner(" ");
-            errorMessage = joiner.add(field).add(violation.getMessage()).toString();
-        } else {
-            errorMessage = "ConstraintViolationException occurred.";
-        }
-
-        return new ResponseEntity<>(getErrorResponse(errorMessage),HttpStatus.BAD_REQUEST);
-    }
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -59,13 +29,6 @@ public class GlobalExceptionHandler  {
                 .collect(Collectors.toList());
         return new ResponseEntity<>(errorDtoList, HttpStatus.BAD_REQUEST);
     }
-
-
-    @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<Object> headerMissingException(MissingRequestHeaderException ex) {
-        return new ResponseEntity<>(getErrorResponse(ex.getMessage()),HttpStatus.BAD_REQUEST);
-    }
-
 
     @ExceptionHandler(GeoLocationClientException.class)
     public ResponseEntity<RegistrationErrorResponseDto> cceCacheException(GeoLocationClientException ex) {
